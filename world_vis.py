fa8d4650f012aec49820b4c7a39a311d841ddf9b
omidhajihassani <ohajihassani@gmail.com>
2019-02-17 21:10:46 +0000

    Move selection update

--- world_vis.py
+++ world_vis.py
@@ -5,21 +5,21 @@ def initialize():
 	board_mesh = np.zeros((6, 7), int);
 	return board_mesh
 
-def is_viable_action(move_i, move_j):
-	if(move_i >= 0 and move_i <= 5) and (move_j >= 0 and move_j <= 6):
-		return True
+def is_viable_action(move_j, board_mesh):
+	if (move_j >= 0 and move_j <= 6):
+		if (board_mesh[5, move_j] == 0):
+			return True
 	else:
 		return False
 
-def get_action(player_number):
+def get_action(player_number, board_mesh):
 	move_j = int(input("Player"+str(player_number)+", Please input the column number of your move from 0 to 6   "));
-	move_i = int(input("Player"+str(player_number)+", Please input the row number of your move from 0 to 5   "));
-	if is_viable_action(move_i, move_j):
+	if is_viable_action(move_j, board_mesh):
 		pass
 	else:
 		print("NOT a Legal Move!!!")
-		move_i, move_j = get_action(player_number)
-	return move_i, move_j;
+		move_j = get_action(player_number, board_mesh)
+	return move_j;
 
 def visualize(board_mesh):
 	i = 5;
@@ -31,11 +31,19 @@ def visualize(board_mesh):
 		print("")
 		i = i - 1;
 		j = 0;
-	
+
+def get_position_on_board(move_j, board_mesh):
+	move_i = 0;
+	for i in range(6):
+		if (board_mesh[i, move_j] == 0):
+			move_i = i
+			break	
+	return move_i;
+
 board_mesh = None;
 live = True;
 turn = 0;
-
+move_i = 0;
 while live:
 	# Initialization
 	if turn == 0:
@@ -43,11 +51,13 @@ while live:
 	# Gameplay
 	if turn%2 == 0:
 		print ("turn 1")
-		move_i, move_j = get_action(turn%2);
+		move_j = get_action(turn%2, board_mesh);
+		move_i = get_position_on_board(move_j, board_mesh);
 		board_mesh[move_i, move_j] = turn%2 + 1
 	else:
 		print ("turn 2") 
-		move_i, move_j = get_action(turn%2);
+		move_j = get_action(turn%2, board_mesh);
+		move_i = get_position_on_board(move_j, board_mesh);
 		board_mesh[move_i, move_j] = turn%2 + 1
 	
 	# Print latest Game Position
