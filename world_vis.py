fad1124b17af3f627ff14a7964b300276022e0f1
omidhajihassani <ohajihassani@gmail.com>
2019-02-17 02:11:03 +0000

    The second instance of the Visualization

--- world_vis.py
+++ world_vis.py
@@ -10,10 +10,10 @@ def is_viable_action(move_i, move_j):
 		return True
 	else:
 		return False
+
 def get_action(player_number):
 	move_j = int(input("Player"+str(player_number)+", Please input the column number of your move from 0 to 6   "));
 	move_i = int(input("Player"+str(player_number)+", Please input the row number of your move from 0 to 5   "));
-	
 	if is_viable_action(move_i, move_j):
 		pass
 	else:
@@ -21,6 +21,17 @@ def get_action(player_number):
 		move_i, move_j = get_action(player_number)
 	return move_i, move_j;
 
+def visualize(board_mesh):
+	i = 5;
+	j = 0;
+	while i != -1:
+		while j != 7:
+			print(str(board_mesh[i, j]), end = ' ', flush=True);
+			j = j + 1;
+		print("")
+		i = i - 1;
+		j = 0;
+	
 board_mesh = None;
 live = True;
 turn = 0;
@@ -32,14 +43,16 @@ while live:
 	# Gameplay
 	if turn%2 == 0:
 		print ("turn 1")
-		get_action(turn%2);
+		move_i, move_j = get_action(turn%2);
+		board_mesh[move_i, move_j] = turn%2 + 1
 	else:
 		print ("turn 2") 
-		get_action(turn%2);
-
-	# Print latest Game Position
-	print(initialize())
+		move_i, move_j = get_action(turn%2);
+		board_mesh[move_i, move_j] = turn%2 + 1
 	
+	# Print latest Game Position
+	visualize(board_mesh)
+
 	turn = turn + 1;
 	
 	# Check end of Game Conditions
