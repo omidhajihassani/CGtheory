0816b0f55b618f13914b88ee23b96dfa136e7a69
Mikael Sabuhi <Mikael.sabuhi@yahoo.com>
2019-02-18 15:01:20 -0700

    In this update, winning condtions are implemented. Winning_Condtion(board_mesh, player_number) Func accepts two input one is the board_mesh which shows the board current look and player_number shows the player's number who did the last move. there are 4 for loops for Horizontal, Vertical, Positive Diagonal and Negative diagonal check of board pieces. output of this function is a boolian variable. This output is used in the main loop of the game to print who wins the game and then stopping the game by changing the live variable to FALSE.

--- world_vis.py
+++ world_vis.py
@@ -11,6 +11,28 @@ def is_viable_action(move_j, board_mesh):
 			return True
 	else:
 		return False
+## Defining the Condition of the winnings
+def winning_Condition(board_mesh,player_number):
+## Checking the Horizontal Winning Strategies
+	for columns in range(0,4):
+		for rows in range (0,6):
+			if board_mesh[rows][columns]==board_mesh[rows][columns+1]==board_mesh[rows][columns+2]==board_mesh[rows][columns+3]==player_number:
+				return True
+## Checking the Vertical Winning Strtegies
+	for columns in range(0,7):
+		for rows in range (0,3):
+			if board_mesh[rows][columns]==board_mesh[rows+1][columns]==board_mesh[rows+2][columns]==board_mesh[rows+3][columns]==player_number:
+				return True
+## Checking the Positive Diagonal Strategies
+	for columns in range(0,4):
+		for rows in range (0,3):
+			if board_mesh[rows][columns]==board_mesh[rows+1][columns+1]==board_mesh[rows+2][columns+2]==board_mesh[rows+3][columns+3]==player_number:
+				return True
+## Checking the Negative Diagonal Strategies
+	for columns in range(0,4):
+		for rows in range (3,6):
+			if board_mesh[rows][columns]==board_mesh[rows-1][columns+1]==board_mesh[rows-2][columns+2]==board_mesh[rows-3][columns+3]==player_number:
+				return True
 
 def get_action(player_number, board_mesh):
 	move_j = int(input("Player"+str(player_number)+", Please input the column number of your move from 0 to 6   "));
@@ -37,7 +59,7 @@ def get_position_on_board(move_j, board_mesh):
 	for i in range(6):
 		if (board_mesh[i, move_j] == 0):
 			move_i = i
-			break	
+			break
 	return move_i;
 
 board_mesh = None;
@@ -54,17 +76,25 @@ while live:
 		move_j = get_action(turn%2, board_mesh);
 		move_i = get_position_on_board(move_j, board_mesh);
 		board_mesh[move_i, move_j] = turn%2 + 1
+
 	else:
-		print ("turn 2") 
+		print ("turn 2")
 		move_j = get_action(turn%2, board_mesh);
 		move_i = get_position_on_board(move_j, board_mesh);
 		board_mesh[move_i, move_j] = turn%2 + 1
-	
+
 	# Print latest Game Position
 	visualize(board_mesh)
-
+	if winning_Condition(board_mesh,1):
+		print("Player 1 Won the Game !")
+		print("Game Over!!")
+		live = False
+	if winning_Condition(board_mesh,2):
+		print("Player 2 Won the Game !")
+		print("Game Over!!")
+		live = False
 	turn = turn + 1;
-	
+
 	# Check end of Game Conditions
 	if turn == 6*7:
 		print ("Game OVER!");
