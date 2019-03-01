d60eb3ceb8aeb3daf05c77b9a2eed48633e3c80c
omidhajihassani <ohajihassani@gmail.com>
2019-02-28 17:16:55 -0700

    Created Testbed for Negamax

--- solver.py
+++ solver.py
@@ -4,10 +4,7 @@ class solver:
     def __init__(self):
         pass
 
-    def position2state(state, move_j):
-    	state.append(move_j);
-
-    def eval_function(board_mesh,player_number):
+    def can_win(board_mesh,player_number):
     	for columns in range(0,4):
     		for rows in range (0,6):
     			if board_mesh[rows][columns]==board_mesh[rows][columns+1]==board_mesh[rows][columns+2]==board_mesh[rows][columns+3]==player_number:
@@ -24,9 +21,6 @@ class solver:
     		for rows in range (3,6):
     			if board_mesh[rows][columns]==board_mesh[rows-1][columns+1]==board_mesh[rows-2][columns+2]==board_mesh[rows-3][columns+3]==player_number:
     				return True
-
-    def eval():
-        pass
 
     def isTerminal(node):
         count = 0
@@ -46,6 +40,14 @@ class solver:
     	else:
     		return False
 
+    def eval_function(node):
+        count = 0
+        for i in range(7):
+            for j in range(6):
+                if node[i, j] != 0:
+                        count = count + 1;
+        return (count + 1 - 42)/2
+
     def get_position_on_board(move_j, board_mesh):
     	move_i = 0;
     	for i in range(6):
@@ -57,19 +59,24 @@ class solver:
     def Negamax(self, node, turn):
         if isTerminal(node):
             return 0;
-
-        for(int x = 0; x < Position::WIDTH; x++) # check if current player can win next move
-            if(P.canPlay(x) && P.isWinningMove(x))
-                return (Position::WIDTH*Position::HEIGHT+1 - P.nbMoves())/2;
-
+        # for(int x = 0; x < Position::WIDTH; x++) # check if current player can win next move
+        #     if(P.canPlay(x) && P.isWinningMove(x))
+        #         return (Position::WIDTH*Position::HEIGHT+1 - P.nbMoves())/2;
+        for i in range(7):
+            if not is_viable_action(i, node):
+                continue;
+            parent = node
+            parent[get_position_on_board(i, node), i] = turn
+            child = parent
+            if can_win(child, player_number):
+                return eval_function(child)
         value = -42
-
         for i in range(7):
             if not is_viable_action(i, node):
                 continue;
             parent = node
             child = (parent[get_position_on_board(i, node), i] = turn)
-            val = max(value, -Negamax(child, -color, (turn)%2))
+            val = max(value, -Negamax(child, (turn)%2))
             if (val > value):
                 value = val
         return value
