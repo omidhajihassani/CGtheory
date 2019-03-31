8a2ded4c7f94b5fc762fcec674962aafdbf80b6e
Mikael Sabuhi <Mikael.sabuhi@yahoo.com>
2019-03-30 20:07:24 -0600

    Adding Game Mode Selection

--- world_vis.py
+++ world_vis.py
@@ -21,6 +21,10 @@ Screen_Height= 600
 Screen_Size = (Screen_Width,Screen_Height)
 Screen = pg.display.set_mode(Screen_Size)
 bg=pg.image.load("Board.png")
+wlc=pg.image.load("Welcome.png")
+wlc1 =pg.image.load("Welcome1.png")
+wlc2 =pg.image.load("Welcome2.png")
+select = pg.image.load("select.png")
 pg.display.set_caption('Connect Four Game')
 piece1 = pg.image.load("piece1.png")
 piece2 = pg.image.load("piece2.png")
@@ -28,6 +32,7 @@ place_holder = pg.image.load("Player_place_holder.png")
 Player1Wins=pg.image.load("Player1Wins.png")
 Player2Wins=pg.image.load("Player2Wins.png")
 Draw=pg.image.load("Drawww.png")
+back = pg.image.load("Back.png")
 Screen.blit(bg,(0,0))
 pg.font.init()
 font=pg.font.SysFont('Helvetica',30)
@@ -35,7 +40,7 @@ text_player1 = font.render("Player 1",False,(0,0,0))
 text_player2 = font.render("Player 2",False,(0,0,0))
 ###########################
 player1_mode = "manual"
-player2_mode = "solver"
+player2_mode = "manual"
 ############
 
 def board_UI(board_mesh):
@@ -162,20 +167,29 @@ def res_Or_quit(restart):
 					Screen.blit(bg,(0,0))
 					pg.display.update()
 					turn = -1
+				if ((event.pos[0]>=700 and event.pos[0]<=800) and ( event.pos[1]>=0 and event.pos[1]<=100)):
+					restart = True
+					board_mesh = initialize()
+					global welcome
+					global once
+					welcome = True
+					once = True
+					turn = -1
+
 			else:
 				pass
 	return board_mesh,turn
 
 				
 
-
+once =True
 board_mesh = initialize()
 live = True
 restart = False
 turn = -1
 move_i = 0
 player = True
-
+welcome = True
 board_UI(board_mesh)
 pg.display.flip()
 
@@ -183,6 +197,76 @@ pg.display.flip()
 
 while live:
 
+	while welcome :
+		while once:
+			Screen.blit(wlc,(0,0))
+			pg.display.update()
+			once = False
+		for event in pg.event.get():
+			if event.type == pg.QUIT:
+				live = False
+				pg.display.quit()
+				pg.quit()
+				sys.exit()
+				#os._exit
+			if event.type == pg.MOUSEBUTTONDOWN : 
+				if ((event.pos[0]>=80 and event.pos[0]<=80+260) and ( event.pos[1]>=280 and event.pos[1]<=330)):
+					player1_mode = "manual"
+					print("Manual")
+					Screen.blit(wlc1,(0,0))
+					Screen.blit(select,(60,280+20))
+					pg.display.update()
+				elif ((event.pos[0]>=80 and event.pos[0]<=80+260) and ( event.pos[1]>=331 and event.pos[1]<=390)):
+					player1_mode = "random"
+					print("Random")
+					Screen.blit(wlc1,(0,0))
+					Screen.blit(select,(60,340+20))
+					pg.display.update()
+				elif ((event.pos[0]>=80 and event.pos[0]<=80+260) and ( event.pos[1]>=391 and event.pos[1]<=460)):
+					player1_mode = "greedy"
+					print("Greedy")
+					Screen.blit(wlc1,(0,0))
+					Screen.blit(select,(60,400+20))
+					pg.display.update()
+				elif ((event.pos[0]>=80 and event.pos[0]<=80+260) and ( event.pos[1]>=461 and event.pos[1]<=510)):
+					player1_mode = "solver"
+					print("Slover")
+					Screen.blit(wlc1,(0,0))
+					Screen.blit(select,(60,460+20))
+					pg.display.update()
+				if ((event.pos[0]>=420 and event.pos[0]<=680) and ( event.pos[1]>=280 and event.pos[1]<=330)):
+					player2_mode = "manual"
+					print("Manual")
+					Screen.blit(wlc2,(400,0))
+					Screen.blit(select,(400,280+20))
+					pg.display.update()
+				elif ((event.pos[0]>=420 and event.pos[0]<=680) and ( event.pos[1]>=331 and event.pos[1]<=390)):
+					player2_mode = "random"
+					print("Random")
+					Screen.blit(wlc2,(400,0))
+					Screen.blit(select,(400,340+20))
+					pg.display.update()
+				elif ((event.pos[0]>=420 and event.pos[0]<=680) and ( event.pos[1]>=391 and event.pos[1]<=460)):
+					player2_mode = "greedy"
+					print("Greedy")
+					Screen.blit(wlc2,(400,0))
+					Screen.blit(select,(400,400+20))
+					pg.display.update()
+				elif ((event.pos[0]>=420 and event.pos[0]<=680) and ( event.pos[1]>=461 and event.pos[1]<=510)):
+					player2_mode = "solver"
+					print("Solver")	
+					Screen.blit(wlc2,(400,0))
+					Screen.blit(select,(400,460+20))
+					pg.display.update()
+				if ((event.pos[0]>=690 and event.pos[0]<=775) and ( event.pos[1]>=485 and event.pos[1]<=572)):
+					print("Play")
+					#Screen.blit(wlc,(0,0))
+					Screen.blit(bg,(0,0))
+					pg.display.update()
+					welcome = False
+					break
+				else :
+					pass
 	if turn == -1:
 		board_mesh = initialize()
 		Screen.blit(piece1,(646,450))
@@ -229,6 +313,8 @@ while live:
 							X_position = event.pos[0]
 							move_j,turn= get_action(turn%2, board_mesh,X_position)
 							player = False
+						else : 
+							pass
 			player = True
 		elif player1_mode =="random":
 			move_j,turn= Random_Action(turn%2,board_mesh)
@@ -238,8 +324,11 @@ while live:
 			#time.sleep(1)
 		elif player1_mode =="solver" :	
 			move_j = slvr.solve(board_mesh, 1, 10) -1
-			turn = turn%2 +1
-
+			if is_viable_action(move_j,board_mesh) :
+				turn = turn%2 +1
+			else : 
+				turn = 1
+		
 		
 		move_i = get_position_on_board(move_j, board_mesh)
 		if is_viable_action(move_j, board_mesh):
@@ -267,6 +356,8 @@ while live:
 							X_position = event.pos[0]
 							move_j,turn= get_action(turn%2, board_mesh,X_position)
 							player = False
+						else:
+							pass
 			player = True
 		elif player2_mode =="random":
 			move_j,turn= Random_Action(turn%2,board_mesh)
@@ -277,7 +368,10 @@ while live:
 
 		elif player2_mode =="solver" :	
 			move_j = slvr.solve(board_mesh, 2, 10) -1
-			turn = turn%2 +1
+			if is_viable_action(move_j,board_mesh) :
+				turn = turn%2 +1
+			else: 
+				turn = 0
 
 		move_i = get_position_on_board(move_j, board_mesh)
 		if is_viable_action(move_j, board_mesh):
@@ -309,6 +403,7 @@ while live:
 		pg.display.update()
 		#time.sleep(5)
 		Screen.blit(Player1Wins,(0,0))
+		Screen.blit(back,(700,10))
 		pg.display.update()
 		#live = False
 		board_mesh,turn=res_Or_quit(restart)	
@@ -324,16 +419,18 @@ while live:
 		#time.sleep(5)
 		#live= False
 		Screen.blit(Player2Wins,(0,0))
+		Screen.blit(back,(700,10))
 		pg.display.update()
 		board_mesh,turn=res_Or_quit(restart)
 
 	#turn = turn + 1;
-
+	
 	# Check end of Game Conditions
 	if 0 not in board_mesh:
 		print ("Game OVER!")
 		#live = False
 		Screen.blit(Draw,(0,0))
+		Screen.blit(back,(700,10))
 		pg.display.update()
 		board_mesh,turn=res_Or_quit(restart)
 		pg.display.update()
